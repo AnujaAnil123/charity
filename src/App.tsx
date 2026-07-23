c64f16eb21ed9f688d9137d74f58d9896ac563a7
ANUJAANIL123 <anuanilcoding@gmail.com>
2026-07-23 17:44:59 +0400

    feat: scaffold React project with Vite, TypeScript, and clinic hub assets

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import ExporttoFigmaClinichubFramerAiClinicHubFamilyClinic from './index'
+
+export default function App() {
+  return (
+    <div style={{ width: '100%', overflowX: 'auto' }}>
+      <ExporttoFigmaClinichubFramerAiClinicHubFamilyClinic />
+    </div>
+  )
+}
